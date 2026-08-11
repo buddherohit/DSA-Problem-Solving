f2966d78c4edf31e01233be4604982408868cb0f
buddherohit <rohitbudddhe564@gmail.com>
2026-08-11 19:46:11 +0530

    Add the comments

--- reverseArray.java
+++ reverseArray.java
@@ -1,12 +1,12 @@
 public class reverseArray {
     static void reverseArray(int arr[]){
-        int n=arr.length;
-        int i=0;
-        int j=n-1;
-        while(i<=j){
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
+        int n=arr.length;     // get the length of the array
+        int i=0;           // initialize the starting index
+        int j=n-1;          // initialize the ending index
+        while(i<=j){              //swap the elements at the starting and ending indices until they meet in the middle 
+            int temp=arr[i];     // store the value of the starting index in a temporary variable
+            arr[i]=arr[j];      // assign the value of the ending index to the starting index
+            arr[j]=temp;      // assign the value of the temporary variable to the ending index
             i++;
             j--;
         }
